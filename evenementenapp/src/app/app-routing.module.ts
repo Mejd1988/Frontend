7989ed06ff56d0ac8bde0a5bd17f21c0c95cd97a
Mejd <mejd1988@gmail.com>
2019-06-13 10:36:30 +0200

    versie 13-06

--- evenementenapp/src/app/app-routing.module.ts
+++ evenementenapp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EvenementComponent } from './evenement/evenement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'evenementen',
+    component: EvenementComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
