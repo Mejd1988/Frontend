9d504a4a7b29fb8c8fa4f66c29fe52a3ad0a4027
Mejd <mejd1988@gmail.com>
2019-06-23 20:05:14 +0200

    artiest toevegen nu mogelijk

--- evenementenapp/src/app/domain/artiest.ts
+++ evenementenapp/src/app/domain/artiest.ts
@@ -1,4 +1,3 @@
 export class Artiest {
-
     naam: string;
 }
--- evenementenapp/src/app/service/artiest.service.ts
+++ evenementenapp/src/app/service/artiest.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Artiest } from '../domain/artiest';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +21,14 @@ export class ArtiestService {
 
   public findAll(): Observable<Artiest[]> {
     return this.httpClient.get<Artiest[]>("http://localhost:8080/artiest");
+  }
+
+  public create(artiest: Artiest): Observable<Artiest> {
+    console.log("In create")
+    return this
+      .httpClient.post<Artiest>(
+        "http://localhost:8080/artiest",
+        artiest,
+        httpOptions)
   }
 }
